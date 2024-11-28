403277ee178fc1477b4718d99e5680c2328d4bd0
Ajith George Sam <121044173+Ajith-Geo@users.noreply.github.com>
2024-11-29 01:56:25 +0530

    Create summarization.py

---
+++ summarization.py
@@ -0,0 +1,9 @@
+def gemini(email: str):
+
+    import google.generativeai as genai
+    genai.configure(api_key='AIzaSyAbjMsZDt2PyIlNONxjp6DVW-TVK91doJE')
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    # email="Dear Team, I recently purchased a pair of headphones (Order #56789), and I'm quite disappointed with the experience. The product arrived late, and the packaging was damaged. Additionally, the headphones are not working as expected—they have a persistent static noise that ruins the sound quality. I was hoping for better service and a functional product, but this experience has been frustrating. Please let me know how this can be resolved quickly. Best regards,John Doe"
+    response = model.generate_content("summarize the following email, only give the output of the summary with all the main insights being considered"+email)
+    # print(response.text)
+    return response.text
